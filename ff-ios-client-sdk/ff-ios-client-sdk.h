8710d2858dbd1d5a453daf8c9d17485d520e8e78
Dusan Juranovic <dusan.juranovic@gmail.com>
2021-02-24 17:16:50 +0100

    Fourth commit

--- ff-ios-client-sdk/ff-ios-client-sdk.h
+++ ff-ios-client-sdk/ff-ios-client-sdk.h
@@ -12,7 +12,7 @@ FOUNDATION_EXPORT double CFiOSClientVersionNumber;
 
 //! Project version string for CFiOSClient.
 FOUNDATION_EXPORT const unsigned char CFiOSClientVersionString[];
-#import <ff-ios-client-sdk/ff-ios-client-sdk.h>
+#import "ff-ios-client-sdk.h"
 // In this header, you should import all the public headers of your framework using statements like #import <ff-ios-client-sdk/PublicHeader.h>
 
 
